5c3deba8f2303237288f13a926648438be92b298
Wojtek <wowaked@gmail.com>
2022-11-10 07:48:39 +0100

    forgot to build the binary. duh.
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 ##/bin/bash
 
+rm sgupdater
 rm sgupdater.zip 
+go build
 zip sgupdater.zip sgupdater 
 aws --profile wpuser --region eu-west-1 lambda update-function-code --function-name SGupdater --zip-file fileb://sgupdater.zip
