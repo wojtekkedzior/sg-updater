f6f47a8a8cb3c2444e4bf9ed38131d1969945c98
Wojtek K <wowaked@gmail.com>
2023-08-27 10:50:54 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ##/bin/bash
 
-rm sgupdater
+rm bootstrap
 rm sgupdater.zip 
-go build
-zip sgupdater.zip sgupdater 
+GOARCH=amd64 GOOS=linux go build -o bootstrap main.go
+zip sgupdater.zip bootstrap 
 aws --profile wpuser --region eu-west-1 lambda update-function-code --function-name SGupdater --zip-file fileb://sgupdater.zip
